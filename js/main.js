8a02c5d0822ee30fb33d191469cac47e8a97ee6c
bqtk0vdev <99440909+BatkovDev@users.noreply.github.com>
2023-12-25 21:04:47 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,23 +23,4 @@ document.addEventListener("DOMContentLoaded", ()=>{
     burgerMenu.classList.toggle('show');
     if(burgerBtn.setAttribute("icon", burgerMenu.classList.contains("show") ? "bi:x" : "bi:list"));
   });
-
-  /*const media = document.querySelectorAll('img');
-  const preloader = document.querySelector('.preloader')
-  const percent = document.querySelector('.preloader__load-percents span');
-  let i = 0;
-  media.forEach((file, index) => {
-    file.onload = () => {
-      i++;
-
-      calcPercent = Math.ceil((i*100) / media.length);
-      if(calcPercent > 100) calcPercent = 100;
-      percent.innerHTML = calcPercent;
-
-      if(i === media.length){
-        percent.innerHTML = 100;
-        preloader.classList.add('hidden');
-      }
-    }
-  })*/
 });
