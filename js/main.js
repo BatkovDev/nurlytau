99bae930b08988cf5688c05fe4cc306fd128e759
bqtk0vdev <99440909+BatkovDev@users.noreply.github.com>
2023-12-26 22:18:14 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
      await ymaps3.ready;
      const {YMap, YMapDefaultSchemeLayer} = ymaps3;
      const map = new YMap(
-         document.getElementById('foo-map'),
+         document.getElementById("foo-map"),
          {
              location: {
                  center: [53.308779, 69.393912],
@@ -16,11 +16,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
      map.addChild(new YMapDefaultSchemeLayer());
   }
-  const burgerBtn = document.querySelector('.burger-btn');
-  const burgerMenu = document.querySelector('.responsive-nav');
+  const burgerBtn = document.querySelector(".burger-btn");
+  const burgerMenu = document.querySelector(".responsive-nav");
 
-  burgerBtn.addEventListener('click', function () {
-    burgerMenu.classList.toggle('show');
-    if(burgerBtn.setAttribute("icon", burgerMenu.classList.contains("show") ? "bi:x" : "bi:list"));
+  burgerBtn.addEventListener("click", function () {
+    burgerMenu.classList.toggle("show");
+    if(burgerBtn.setAttribute("icon", busrgerMenu.classList.contains("show") ? "bi:x" : "bi:list"));
+  });
+
+  document.querySelector(".botbtn__contacts-modal").addEventListener("click", function () {
+    document.querySelector(".mobile__contacts").classList.toggle("open");
+  });
+
+  document.querySelector(".mobile__contacts-modal .modal-close").addEventListener("click", function () {
+    document.querySelector(".mobile__contacts").classList.remove("open");
+  });
+
+  document.querySelector(".mobile__contacts").addEventListener("click", function (event) {
+    if (!event.target.closest(".mobile__contacts-modal")) {
+      document.querySelector(".mobile__contacts").classList.remove("open");
+    }
   });
 });
