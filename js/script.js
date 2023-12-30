7ba53edd2f45e2fcc348b927d9f44c65a56b190d
bqtk0vdev <99440909+BatkovDev@users.noreply.github.com>
2023-12-30 14:46:17 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,30 +12,40 @@ $(document).ready(function (){
       }
   });
   
-  const ImagePaths = {imgcontainer: ["ris12.png","ris2.png","ris3.png"],
-                      aboutslider: ["ris9.jpg","ris8.jpg"]
+  const ImagePaths = {
+    imgcontainer: ["ris12.png", "ris2.png", "ris3.png"],
+    aboutslider: ["ris9.jpg", "ris8.jpg"]
   };
-  const Activity_spans = ["Монтаж металлоконструкций","Устройство фундамента","Монтаж сэндвич-панелей"];
+  const Activity_spans = ["Монтаж металлоконструкций", "Устройство фундамента", "Монтаж сэндвич-панелей"];
   let currentIndex1 = 0;
   let currentIndex2 = 0;
 
-  function chImageSRC(){
+
+  function chImageSRC() {
     /* IMG CONTAINER */
     $("#progress_fill").stop().css("width", 0);
-    $(".overlay-bot .overlay-span span").text(Activity_spans[currentIndex1]).fadeOut(100).fadeIn(100);
-    $("#m_iimg").attr("src", "img/"+ImagePaths.imgcontainer[currentIndex1]);
-    currentIndex1 = (currentIndex1+1) % ImagePaths.imgcontainer.length;
+
+    $(".overlay-bot .overlay-span span").fadeOut(100, function() {
+      $(this).text(Activity_spans[currentIndex1]).fadeIn(100);
+    });
+
+    $("#m_iimg").fadeOut(100, function() {
+      $(this).attr("src", "img/" + ImagePaths.imgcontainer[currentIndex1]).fadeIn(100);
+    });
+
+    currentIndex1 = (currentIndex1 + 1) % ImagePaths.imgcontainer.length;
+
     $("#progress_fill").animate({
       width: "100%"
     }, 5000, "linear");
 
     /* ABOUT SLIDER */
-    $(".about-us__slider-img #img-container__item").attr("src", "img/"+ImagePaths.aboutslider[currentIndex2]);
-    /*console.log(currentIndex2);
-    console.log(ImagePaths.aboutslider[currentIndex2]);*/
-    currentIndex2 = (currentIndex2+1) % ImagePaths.aboutslider.length;
+    $(".about-us__slider-img #img-container__item").fadeOut(100, function() {
+      $(this).attr("src", "img/" + ImagePaths.aboutslider[currentIndex2]).fadeIn(100);
+    });
+
+    currentIndex2 = (currentIndex2 + 1) % ImagePaths.aboutslider.length;
   }
 
   setInterval(chImageSRC, 5000);
-
 });
