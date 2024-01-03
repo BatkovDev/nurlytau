85ec1682c9a580a9cfe82751baf766c07196071b
Akurochek <154026245+Akurochek@users.noreply.github.com>
2024-01-03 20:53:54 +0600

    Update index.js

--- input/index.js
+++ input/index.js
@@ -1,9 +1,14 @@
 const input = document.querySelector('.inpt01');
 input.addEventListener("change", (event) => {
     let i=input.value.replace(/[^0-9]/g, '');
-    input.value=i+" м";
+    
+    if (i !== "") {
+        input.value=i+" м";
+    }
     console.log(input.value);
     
     
+    
 });
+
 
