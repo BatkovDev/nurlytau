235b00a3f685d0c9db4271eeeb0140dd4ccb97eb
Akurochek <154026245+Akurochek@users.noreply.github.com>
2024-01-03 20:42:02 +0600

    Add files via upload

---
+++ input/index.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('.inpt01');
+input.addEventListener("change", (event) => {
+    let i=input.value.replace(/[^0-9]/g, '');
+    input.value=i+" м";
+    console.log(input.value);
+    
+    
+});
+
