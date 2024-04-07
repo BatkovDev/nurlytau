c57c8ec82b99c63566b810385d0d49c0687ebf1c
bqtk0vdev <99440909+BatkovDev@users.noreply.github.com>
2024-04-07 14:40:01 +0500

    Add files via upload

---
+++ ntnew/js/header-script.js
@@ -0,0 +1,74 @@
+const swiperHeader = new Swiper('#home .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 8000, // Автоматическая прокрутка слайдов через 5 секунд
+    disableOnInteraction: false, // Отключение автовоспроизведения при взаимодействии пользователя
+  },
+
+  direction: 'horizontal',
+  speed: 300,
+  effect: "fade",
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+const swiperAbout = new Swiper('#about .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 5000, // Автоматическая прокрутка слайдов через 5 секунд
+    disableOnInteraction: false, // Отключение автовоспроизведения при взаимодействии пользователя
+  },
+
+  direction: 'horizontal',
+  speed: 300,
+  effect: "fade",
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+new Swiper('.projectsSwiper', {
+  loop: true,
+
+  direction: 'horizontal',
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+  }
+});
+
+const burgerMenuOpen = document.querySelector("#header .burger-btn");
+const burgerMenuClose = document.querySelector("#header .burger-close");
+
+burgerMenuOpen.addEventListener("click", () => {
+  document.querySelector("#header .links").classList.toggle("show");
+});
+burgerMenuClose.addEventListener("click", () => {
+  document.querySelector("#header .links").classList.remove("show");
+});
